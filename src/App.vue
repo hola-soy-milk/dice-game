ea0c30fc82843f4be7f6521231cd12145d3eb347
Ramón Huidobro <hola@ramonh.dev>
2021-11-02 13:05:55 +0100

    Add dice rolling

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import Die from './components/Die.vue'
+const roll = () => Array.from({length: 6}, () => Math.floor(Math.random() * 6) + 1);
+const dice = ref(roll());
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="flex center">
+    <Die v-for="die in dice" :faceValue="die"/>
+  </div>
+  <button type="button" @click="dice = roll()">Reroll</button>
 </template>
 
 <style>
@@ -17,5 +22,11 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.flex {
+  display: flex;
+}
+.center {
+  justify-content: center;
 }
 </style>
