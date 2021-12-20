49d0d39e619b01b173ef2f202496415e4b4c1fdd
Ramón Huidobro <hola@ramonh.dev>
2021-12-20 16:47:53 +0100

    Add styles, fix

--- src/App.vue
+++ src/App.vue
@@ -9,27 +9,56 @@ const dice = ref(roll());
 </script>
 
 <template>
-  <h1>Let's have a game of twenty-one!</h1>
-  <div class="flex center">
-    <Die v-for="die in dice" :faceValue="die.value()"/>
+  <div>
+    <h1 class="top">¡Juguemos al 21!</h1>
+    <div class="flex center">
+      <Die v-for="die in dice" :faceValue="die.value()"/>
+    </div>
   </div>
-  <button type="button" @click="dice = roll()">Reroll</button>
   <h1>
     {{dice.reduce((sum, die) => sum + die.value(), 0)}}
     <span v-if="dice.reduce((sum, die) => sum + die.value(), 0) < 21">🎉</span>
     <span v-else>😥</span>
   </h1>
+  <button type="button" @click="dice = roll()">Reroll</button>
 </template>
 
 <style>
+body, html {
+  height: 100%;
+  width: 100%;
+  background-color: #f0eeef;
+  margin: 0;
+  }
 #app {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #025b0e;
 }
+.top {
+  margin-top: 60px;
+  }
+button {
+  background-color: #fd7c84;
+  color: #f0eeef;
+  border: none;
+  font-size: 2em;
+  font-weight: 600;
+  padding: 1em 2em;
+  cursor: pointer;
+  }
+button:hover {
+  filter: brightness(115%);
+  }
+button:active {
+  filter: brightness(85%);
+  }
 .flex {
   display: flex;
 }
